128e79ab85812a3e1e69435d11dd7ff853e4d031
AnastasiiaPhoenix <108626826+AnastasiiaPhoenix@users.noreply.github.com>
2022-08-27 16:24:28 +0300

    API for forecast

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
       hours = `0${hours}`;
@@ -7,7 +8,6 @@ function formatDate(date) {
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
-    let dayOftheWeek = date.getDay();
   
     let days = [
       "Sunday",
@@ -18,15 +18,53 @@ function formatDate(date) {
       "Friday",
       "Saturday"
     ];
-    let day = days[dayOftheWeek];
+    let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
   }
-  
+
+  function formatDay(timestamp) {
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+return days[day];
+  }
+
   let currentDate = document.querySelector("#date");
   let currentTime = new Date();
   currentDate.innerHTML = formatDate(currentTime);
-  
-  //
+
+  function displayForecast(response) {
+    let forecast = response.data.daily;
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col-2">
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+          <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+          <div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+            <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
+          </div>
+        </div>
+    `;
+    }
+  });
+    
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+  }
+
+function getForecast(coordinates) {
+  let apiKey = "f5f2cd381e5593f6659618e002cc4ae6";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayForecast);
+}
+
   function showWeather(response) {
     console.log(response.data);
     document.querySelector("#user-city").innerHTML = response.data.name;
@@ -42,6 +80,7 @@ function formatDate(date) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
   }
   function searchCity(city) {
     let apiKey = "f5f2cd381e5593f6659618e002cc4ae6";
@@ -57,7 +96,6 @@ function formatDate(date) {
   let form = document.querySelector(".citysearch");
   form.addEventListener("submit", search);
   
-  //
   function localWeather(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
@@ -70,4 +108,3 @@ function formatDate(date) {
     navigator.geolocation.getCurrentPosition(localWeather);
   }
   searchCity("Zaporizhzhia");
-  
